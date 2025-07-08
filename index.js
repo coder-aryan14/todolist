137a4b596cb9f45cdf58603ac545c1e644e9f713
coder-aryan14 <aryankhedulkar@gmail.com>
2025-07-08 14:48:00 +0530

    port number assigned

--- index.js
+++ index.js
@@ -38,4 +38,6 @@ app.post("/edit/:index", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(8000, () => console.log("Server started on http://localhost:8000"));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
